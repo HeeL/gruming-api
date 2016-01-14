27d322444ab16b5eb93f96a0533a15b56e2b7369
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2016-01-14 21:12:01 +0100

    add error handlers and loggers

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import helmet from 'helmet';
+import { logErrors, errorHandler } from './lib/errorHandler';
+import words from './routes/v1/words';
+
+const app = express();
+
+// security
+app.use(helmet());
+app.disable('x-powered-by');
+
+// static
+app.use(express.static(__dirname + '../public'));
+
+// api routes
+app.use('/v1/words', words);
+
+// logs, error handling
+app.use(logErrors);
+app.use(errorHandler);
+
+export default app;
