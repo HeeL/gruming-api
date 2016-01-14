edd299ae1bdd4d90d87d0689993d23a1265cc1dc
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2016-01-14 20:11:55 +0100

    add helmet for security reasons

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import words from './routes/v1/words';
+import helmet from 'helmet';
 
 const app = express();
+app.use(helmet());
+app.disable('x-powered-by');
 app.use(express.static(__dirname + '/public'));
 app.use('/v1/words', words);
 
