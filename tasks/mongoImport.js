669c5191db9d1139dc4ed7fb100a5c053964f166
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2016-01-15 17:17:50 +0100

    change pass env var to namespace with mongodb

--- tasks/mongoImport.js
+++ tasks/mongoImport.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 
 if (process.env.NODE_ENV == 'production') {
-    exec(`mongoimport -d ${process.env.MONGODB_NAME} -u ${process.env.MONGODB_USER} -p ${process.env.PASS}
+    exec(`mongoimport -d ${process.env.MONGODB_NAME} -u ${process.env.MONGODB_USER} -p ${process.env.MONGODB_PASS}
          -h ${process.env.MONGODB_HOST} --drop -c words --jsonArray data/words.json`, console.log);
 }
 else {
